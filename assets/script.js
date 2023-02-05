3602c79c5d93aaf220fcec2c61f9a54d372608b2
sophiemarisa <sophiecharlwood@live.co.uk>
2023-02-05 20:40:14 +0000

    setting the max wallet key saves to 5

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,6 @@ var preSave = JSON.parse(localStorage.getItem("walletSave"));
 if (preSave === null) {
     console.log("empty")
 }
-
 else {
     preSaveAppend(preSave);
 
@@ -30,7 +29,7 @@ $(document).ready(function () {
             var safe = response.result.SafeGasPrice
             var proposed = response.result.ProposeGasPrice
             var fast = response.result.FastGasPrice
-            console.log(safe + " " + proposed + " " + fast)
+            //console.log(safe + " " + proposed + " " + fast)
 
             $("#safeGas").empty()
             $("#proposedGas").empty()
@@ -55,77 +54,48 @@ $(document).ready(function () {
 
 //checks for the yes button on the modal being clicked
 $("#modalYes").on("click", function () {
-    //clear wallet key input
+
+        //gets the wallet key that was inputted
+        var keyValue = document.getElementById("wallet-key").value;
 
 
-    //gets the wallet key that was inputted
-    var keyValue = document.getElementById("wallet-key").value;
+        //var walletKeyTest = "0xf5fC2431947f214995eFc4Bb6ED6dea09e968828"
+        var walletURL = "https://api.etherscan.io/api?module=account&action=balance&address=" + keyValue + "&tag=latest&apikey=" + apiKey
 
-
-    //var walletKeyTest = "0xf5fC2431947f214995eFc4Bb6ED6dea09e968828"
-    var walletURL = "https://api.etherscan.io/api?module=account&action=balance&address=" + keyValue + "&tag=latest&apikey=" + apiKey
-
-    var walletSave = {
-        walletKey: keyValue,
-    }
-
-    var prevSave = JSON.parse(localStorage.getItem("walletSave") || '[]');
-
-    //console.log(preSave)
-
-    prevSave.push(walletSave);
-    localStorage.setItem("walletSave", JSON.stringify(prevSave));
-
-    var preSaveUpdate = JSON.parse(localStorage.getItem("walletSave"))
-
-    //console.log(preSaveUpdate)
-
-
-    preSaveAppend(preSaveUpdate)
-
-    //AJAX TARGETING WALLET BALANCE
-    $.ajax({
-        url: walletURL,
-        method: "GET"
-    }).then(function (response) {
-        // console.log(response)
-        var weiResult = response.result
-
-        var ethBalance = (weiResult / 1000000000000000000).toFixed(4)
-        //console.log(ethBalance)
-        //display balance 
-        $("#balanceDisplay").append(ethBalance)
-
-    });
-
-    function preSaveAppend() {
-        //looping through the presave array
-        for (i = 0; i < preSave.length; i++) {
-            //for every position in the presave array, it will look into the array and get the position value
-            var walletKeyId = preSave[i].walletKey;
-            //console.log(walletKeyId)
-    
-            var buttonPosition = i + 1;
-    
-            if (buttonPosition === 1) {
-                $("#1").append(walletKeyId)
-            } else if (buttonPosition === 2) {
-                $("#2").append(walletKeyId)
-            }
-            else if (buttonPosition === 3) {
-                $("#3").append(walletKeyId)
-            }
-            else if (buttonPosition === 4) {
-                $("#4").append(walletKeyId)
-            }
-            else if (buttonPosition === 5) {
-                $("#5").append(walletKeyId)
-            }
+        var walletSave = {
+            walletKey: keyValue,
         }
-    }
-    //calling the function here
-    preSaveAppend();
-    
+
+        var prevSave = JSON.parse(localStorage.getItem("walletSave") || '[]');
+
+        //console.log(preSave)
+
+        prevSave.push(walletSave);
+        localStorage.setItem("walletSave", JSON.stringify(prevSave));
+
+        var preSaveUpdate = JSON.parse(localStorage.getItem("walletSave"))
+
+        //console.log(preSaveUpdate)
+
+
+        preSaveAppend(preSaveUpdate)
+
+        //AJAX TARGETING WALLET BALANCE
+        $.ajax({
+            url: walletURL,
+            method: "GET"
+        }).then(function (response) {
+            // console.log(response)
+            var weiResult = response.result
+
+            var ethBalance = (weiResult / 1000000000000000000).toFixed(4)
+            //console.log(ethBalance)
+            //display balance 
+            $("#balanceDisplay").append(ethBalance)
+
+        })
+    ;
+
     //saved the key to the local storage
     localStorage.setItem("walletKey", keyValue);
 
@@ -156,7 +126,7 @@ $("#modalNo").on("click", function () {
         //console.log(ethBalance)
         //display balance 
 
-        
+
 
         //if (console.log($("#balanceDisplay").text()) === "Euthereum Balance:") {
 
@@ -170,7 +140,7 @@ $("#modalNo").on("click", function () {
 
 function preSaveAppend(preSaveArray) {
 
-
+    console.log(preSaveArray.length)
     if (preSaveArray === null) {
         console.log(null);
     }
@@ -198,29 +168,41 @@ function preSaveAppend(preSaveArray) {
             else if (buttonPosition === 5 & $('#5').is(':empty')) {
                 $("#5").append(walletKeyId)
             }
+
         }
     }
 
-$("#go-button").on("click", function() {
-    $("#balanceDisplay").empty()
-})
+    $("#go-button").on("click", function () {
+        $("#balanceDisplay").empty();
 
+        var preSaveUpdate = JSON.parse(localStorage.getItem("walletSave"))
 
+        if (preSaveUpdate === null){
+            $("#maxSave").css("display","none");
+            $("#modalYes").attr("disabled", false)
+        }
+        else if (preSaveUpdate.length >= 5){
+            $("#maxSave").css("display","block");
+            $("#modalYes").attr("disabled", true)
+        }
+        else if (preSaveUpdate.length < 5){
+            $("#maxSave").css("display","none");
+            $("#modalYes").attr("disabled", false)
+        }
 
+    })
 }
-
-
 
 
 //clear button
 
 $("#clearBtn").on("click", function () {
     localStorage.clear();
-     $("#1").empty();
-     $("#2").empty();
-     $("#3").empty();
-      $("#4").empty();
-      $("#5").empty();
+    $("#1").empty();
+    $("#2").empty();
+    $("#3").empty();
+    $("#4").empty();
+    $("#5").empty();
 
 });
 
